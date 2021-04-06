4d8d7281ade368176abe92682d344275309d8a73
mesutAygun1881 <mesutaygun35@gmail.com>
2021-04-06 13:08:00 +0300

    Initial Commit

---
+++ landmarkBook/landmarkBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkBookApp.swift
+//  landmarkBook
+//
+//  Created by mesutAygun on 6.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct landmarkBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
